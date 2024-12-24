6d2b97eb2870da47c5cac16955bfde38406f38a8
hapeeeeee <623151737@qq.com>
2024-12-24 14:21:48 +0000

    support burn

--- programs/ppl/src/lib.rs
+++ programs/ppl/src/lib.rs
@@ -5,16 +5,20 @@ use anchor_spl::{
         create_metadata_accounts_v3, mpl_token_metadata::types::DataV2, CreateMetadataAccountsV3,
         Metadata as Metaplex,
     },
-    token::{mint_to, Mint, MintTo, Token, TokenAccount},
+    token::{burn, mint_to, Burn, Mint, MintTo, Token, TokenAccount},
 };
-declare_id!("FUFwgXFAQvRjKa7qQHcb4JBRrpZj6NZ87pfWYMDRhKex");
+declare_id!("9kSWmDyFUrNjkxE41edjXkrJYLt1YD5XWMMpYqcPj9nc");
 
 #[program]
 mod spl {
     use super::*;
 
     pub fn initiate_token(_ctx: Context<InitToken>, metadata: InitTokenParams) -> Result<()> {
-        let seeds = &["mint".as_bytes(), &[_ctx.bumps.mint]];
+        let seeds = &[
+            "mint".as_bytes(),
+            metadata.id.as_bytes(),
+            &[_ctx.bumps.mint],
+        ];
         let signer = [&seeds[..]];
 
         let token_data: DataV2 = DataV2 {
@@ -47,8 +51,8 @@ mod spl {
         Ok(())
     }
 
-    pub fn mint_tokens(ctx: Context<MintTokens>, quantity: u64) -> Result<()> {
-        let seeds = &["mint".as_bytes(), &[ctx.bumps.mint]];
+    pub fn mint_tokens(ctx: Context<MintTokens>, params: MintTokenParams) -> Result<()> {
+        let seeds = &["mint".as_bytes(), params.id.as_bytes(), &[ctx.bumps.mint]];
         let signer = [&seeds[..]];
 
         mint_to(
@@ -61,12 +65,47 @@ mod spl {
                 },
                 &signer,
             ),
-            quantity,
+            params.quantity,
         )?;
 
         Ok(())
     }
 
+    pub fn burn_tokens(ctx: Context<BurnTokens>, params: BurnTokenParams) -> Result<()> {
+        let cpi_program = ctx.accounts.token_program.to_account_info();
+        let cpi_accounts = Burn {
+            mint: ctx.accounts.mint.to_account_info(),
+            from: ctx.accounts.token_account.to_account_info(),
+            authority: ctx.accounts.payer.to_account_info(),
+        };
+
+        let seeds = &["mint".as_bytes(), params.id.as_bytes(), &[ctx.bumps.mint]];
+        let signer = [&seeds[..]];
+
+        let cpi_ctx = CpiContext::new_with_signer(cpi_program, cpi_accounts, &signer);
+        burn(cpi_ctx, params.quantity)?;
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+#[instruction(params: BurnTokenParams)]
+pub struct BurnTokens<'info> {
+    #[account(
+        mut,
+        seeds = [b"mint", params.id.as_bytes()],
+        bump,
+        mint::authority = mint,
+    )]
+    pub mint: Account<'info, Mint>,
+    #[account(mut)]
+    pub token_account: Account<'info, TokenAccount>,
+    #[account(mut)]
+    pub payer: Signer<'info>,
+    pub rent: Sysvar<'info, Rent>,
+    pub system_program: Program<'info, System>,
+    pub token_program: Program<'info, Token>,
+    pub associated_token_program: Program<'info, AssociatedToken>,
 }
 
 #[derive(Accounts)]
@@ -77,7 +116,7 @@ pub struct InitToken<'info> {
     pub metadata: UncheckedAccount<'info>,
     #[account(
         init,
-        seeds = [b"mint"],
+        seeds = [b"mint", params.id.as_bytes()],
         bump,
         payer = payer,
         mint::decimals = params.decimals,
@@ -93,10 +132,11 @@ pub struct InitToken<'info> {
 }
 
 #[derive(Accounts)]
+#[instruction(params: MintTokenParams)]
 pub struct MintTokens<'info> {
     #[account(
         mut,
-        seeds = [b"mint"],
+        seeds = [b"mint", params.id.as_bytes()],
         bump,
         mint::authority = mint,
     )]
@@ -122,6 +162,19 @@ pub struct InitTokenParams {
     pub symbol: String,
     pub uri: String,
     pub decimals: u8,
+    pub id: String,
+}
+
+#[derive(AnchorSerialize, AnchorDeserialize, Debug, Clone)]
+pub struct MintTokenParams {
+    pub id: String,
+    pub quantity: u64,
+}
+
+#[derive(AnchorSerialize, AnchorDeserialize, Debug, Clone)]
+pub struct BurnTokenParams {
+    pub id: String,
+    pub quantity: u64,
 }
 
 pub struct Initialize {}
--- tests/ppl.ts
+++ tests/ppl.ts
@@ -4,6 +4,17 @@ import assert from "assert";
 import BN from "bn.js";
 import { Spl } from "../target/types/spl";
 
+const payerPair = web3.Keypair.fromSecretKey(
+  new Uint8Array([
+    203, 167, 69, 177, 11, 31, 89, 175, 115, 48, 165, 211, 252, 68, 104, 115, 1,
+    105, 35, 88, 95, 130, 94, 201, 230, 101, 183, 53, 147, 220, 81, 124, 22,
+    159, 194, 135, 43, 204, 30, 103, 174, 167, 82, 123, 164, 91, 140, 50, 31,
+    126, 126, 115, 63, 33, 87, 219, 86, 67, 172, 210, 181, 57, 241, 219,
+  ])
+);
+
+const DIFF_SEED = "3Zax3";
+
 describe("spl program test", () => {
   // Configure the client to use the local cluster.
   anchor.setProvider(anchor.AnchorProvider.env());
@@ -15,21 +26,22 @@ describe("spl program test", () => {
     "metaqbxxUerdq28cj1RbAWkYQm3ybzjb6a8bt518x1s"
   );
 
-  const MINT_SEED = "mint";
+  const MINT_SEED = DIFF_SEED;
   const payer = program.provider.publicKey;
   const metadata = {
-    name: "Net2Dev SPL Rewards Token",
-    symbol: "N2DR",
+    name: DIFF_SEED,
+    symbol: DIFF_SEED,
     uri: "https://arweave.net/Xjqaj_rYYQGrsiTk9JRqpguA813w6NGPikcRyA1vAHM",
-    decimals: 9
-  }
+    decimals: 9,
+    id: DIFF_SEED,
+  };
   const mintAmount = 10;
 
-  const [mint] = web3.PublicKey.findProgramAddressSync(
-    [Buffer.from(MINT_SEED)],
+  const [mint, bump] = web3.PublicKey.findProgramAddressSync(
+    [Buffer.from("mint"), Buffer.from(metadata.id)],
     program.programId
   );
-
+  console.log("mint address: ", mint.toBase58());
   const [metadataAddress] = web3.PublicKey.findProgramAddressSync(
     [
       Buffer.from(METADATA_SEED),
@@ -56,10 +68,10 @@ describe("spl program test", () => {
       tokenMetadataProgram: TOKEN_METADATA_PROGRAM_ID,
     };
 
-
     const txHash = await program.methods
       .initiateToken(metadata)
       .accounts(context)
+      .signers([])
       .rpc();
 
     await program.provider.connection.confirmTransaction(txHash, "finalized");
@@ -77,12 +89,19 @@ describe("spl program test", () => {
     let initialBalance: number;
 
     try {
-      const balance = await program.provider.connection.getTokenAccountBalance(destination);
+      const balance = await program.provider.connection.getTokenAccountBalance(
+        destination
+      );
       initialBalance = balance.value.uiAmount;
     } catch {
       // Token account not yet initiated has 0 balance
       initialBalance = 0;
     }
+
+    const mint_tokens_params = {
+      quantity: new BN(mintAmount * 10 ** metadata.decimals),
+      id: DIFF_SEED,
+    };
 
     const context = {
       mint,
@@ -95,7 +114,7 @@ describe("spl program test", () => {
     };
 
     const txHash = await program.methods
-      .mintTokens(new BN(mintAmount * 10 ** metadata.decimals))
+      .mintTokens(mint_tokens_params)
       .accounts(context)
       .rpc();
     await program.provider.connection.confirmTransaction(txHash);
@@ -110,7 +129,56 @@ describe("spl program test", () => {
       "Compare balances, it must be equal"
     );
   });
+
+  it("burn tokens", async () => {
+    const token_account = await anchor.utils.token.associatedAddress({
+      mint: mint,
+      owner: payer,
+    });
+
+    let burnBalance: number;
+
+    try {
+      const balance = await program.provider.connection.getTokenAccountBalance(
+        token_account
+      );
+      burnBalance = balance.value.uiAmount / 2;
+      console.log("balance: ", balance);
+    } catch {
+      // Token account not yet initiated has 0 balance
+      burnBalance = 0;
+    }
+
+    const burn_tokens_params = {
+      quantity: new BN(burnBalance * 10 ** metadata.decimals),
+      id: DIFF_SEED,
+    };
+
+    const context = {
+      mint: mint,
+      tokenAccount: token_account,
+      payer: payer,
+      rent: web3.SYSVAR_RENT_PUBKEY,
+      systemProgram: web3.SystemProgram.programId,
+      tokenProgram: anchor.utils.token.TOKEN_PROGRAM_ID,
+      associatedTokenProgram: anchor.utils.token.ASSOCIATED_PROGRAM_ID,
+    };
+
+    const txHash = await program.methods
+      .burnTokens(burn_tokens_params)
+      .accounts(context)
+      .signers([payerPair])
+      .rpc();
+    await program.provider.connection.confirmTransaction(txHash);
+    console.log(`  https://explorer.solana.com/tx/${txHash}?cluster=devnet`);
+
+    const postBalance = (
+      await program.provider.connection.getTokenAccountBalance(token_account)
+    ).value.uiAmount;
+    assert.equal(
+      burnBalance,
+      postBalance,
+      "Compare balances, it must be equal"
+    );
+  });
 });
-
-
-
