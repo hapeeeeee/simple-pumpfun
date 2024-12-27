58701000b542a7d13fb1fdeaaaa43d7a9dce6e31
hapeeeeee <623151737@qq.com>
2024-12-27 09:50:39 +0800

    add swapinput and swapoutput

--- programs/ppl/src/instructions/mod.rs
+++ programs/ppl/src/instructions/mod.rs
@@ -5,6 +5,9 @@ pub mod mint_token;
 pub mod burn_token;
 pub mod proxy_initialize;
 pub mod proxy_deposit;
+pub mod proxy_swap_base_input;
+pub mod proxy_swap_base_output;
+
 // pub use initialize::*;
 // pub use create_pool::*;
 pub use create_token::*;
@@ -12,3 +15,5 @@ pub use mint_token::*;
 pub use burn_token::*;
 pub use proxy_initialize::*;
 pub use proxy_deposit::*;
+pub use proxy_swap_base_input::*;
+pub use proxy_swap_base_output::*;
